3fcd8e2792ba78116550dd6518ad359b5aa59e83
Ben Iofel <ben@iofel.me>
2024-03-31 01:41:40 -0400

    more ui stuff

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 
 (scroll-bar-mode 1)
 (tooltip-mode -1)
-(tool-bar-mode 1) ;; -1 = disable, 1 = enable ?
+(tool-bar-mode -1) ;; -1 = disable, 1 = enable ?
 (set-fringe-mode 10)
 
 (menu-bar-mode -1)
@@ -10,8 +10,6 @@
 (setq visible-bell t)
 
 (set-face-attribute 'default nil :font "Operator Mono Book" :height 140)
-
-(load-theme 'wombat)
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
@@ -27,6 +25,17 @@
   (package-refresh-contents))
 
 (setq use-package-always-ensure t)
+
+;; line number
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; logs all key presses and functions
 (use-package command-log-mode)
@@ -47,3 +56,42 @@
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 30)))
+
+;; rainbow delimiters
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; show which keys you can press next
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config (setq which-key-idle-delay 0))
+
+;; add details to vertico menus
+(use-package marginalia
+  :bind (:map minibuffer-local-map ("M-a" . marginalia-cycle))
+  :init (marginalia-mode))
+
+;; better help system
+(use-package helpful
+  :bind
+  ([remap describe-function] . helpful-callable)
+  ([remap describe-variable] . helpful-variable)
+  ([remap describe-key] . helpful-key)
+  ([remap describe-command] . helpful-command))
+
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-gruvbox t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
